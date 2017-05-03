0b662f1e9f9888f56b8d52992a824d7ef60658bd
jhonedarts <jhonedarts@gmail.com>
2017-05-03 09:31:42 -0300

    centroide, separação feita e checagem na stoplist.
    Falta checar os pesos

--- Robô/src/model/Parser.java
+++ Robô/src/model/Parser.java
@@ -6,6 +6,7 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,12 +21,31 @@ import org.jsoup.select.Elements;
  * @author Jhansen
  */
 public class Parser {
+    private String tags[] = {"p","h3","head"};
     private ArrayList<Site> sites;
     private Document doc;
-    private HashMap<String, Boolean> semes;//nem precisa da lista de seeds
+    private HashMap<String, Boolean> stoplist;//nem precisa da lista de seeds
     private final ArrayList<Semente> seeds;
     
-    public Parser(){
+    public Parser() throws IOException{
+        BufferedReader sourceBr = new BufferedReader(new FileReader(new File("stoplist.txt")));
+        String line;
+        stoplist = new HashMap();
+        while ((line = sourceBr.readLine()) != null) {
+            line = line.trim();
+            if(!line.equals("")){//linha vazia
+                stoplist.put(line, Boolean.FALSE);
+            }
+        }
+        sourceBr.close();
+        sourceBr = new BufferedReader(new FileReader(new File("stoplistEn.txt")));
+        while ((line = sourceBr.readLine()) != null) {
+            line = line.trim();
+            if(!line.equals("")){//linha vazia
+                stoplist.put(line, Boolean.FALSE);
+            }
+        }
+        sourceBr.close();
         this.seeds = new ArrayList<>();
     }
     /**
@@ -67,6 +87,7 @@ public class Parser {
             for (Element link : links) {
                 System.out.println(link.attr("abs:href")+"  -  "+link.text().trim());
             }
+            getCentroide(doc.text());
             this.seeds.add(new Semente(url, true));
         } catch (IOException ex) {
             System.err.println("Erro: " + ex);
@@ -75,5 +96,30 @@ public class Parser {
     
     public void imprimeLista(){
         System.out.println(seeds.toString());
+    }
+    
+    private void getCentroide(String txt){
+        HashMap<String, Centroide> words = new HashMap();
+        String palavras[] = txt.split("\\s+|\\n+|\\t+|\\r+|\\.+|\\,+|\\;+|\\:+|\\(+|\\)+|#+|\"+|'+|[+|]+|");
+                //+ "issimo|íssimo|issimos|íssimos|issima|íssima|issimas|íssimas|manha|manhã|noite|exemplo|minutos|segundos");
+        for(String str:palavras){
+            str =str.trim();
+            Centroide cent = words.get(str);
+            Boolean stopLContains = stoplist.get(str);
+            if (!stopLContains){ //se nao está na stoplist
+                if(cent==null){//se não esta na lista de centroides
+                    cent = new Centroide(str, 0, 1);
+                    words.put(str, cent);
+                    System.out.println(str);
+                }else{// se já esta na lista de centroides, incrementa a ocorrencia
+                    cent.setOcorrencia(cent.getOcorrencia()+1);
+                    System.out.println(cent.getConteudo()+" - "+cent.getOcorrencia());
+                }
+            }
+            //hasmap de words populada, vamos agora atribuir os pesos
+            for(int i=0;i<tags.length;i++){
+                String aux[] = doc.//receber um vetor de Strings das palavras que tem na tag html na posição i de tags
+            }
+        }
     }
 }
